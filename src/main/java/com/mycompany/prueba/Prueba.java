5f6a79d6be1328f6dcd6660c2748e33a1de24609
Fati3 <fatigueee56@gmail.com>
2025-06-30 14:48:10 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/prueba/Prueba.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author guefr
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+                new login().setVisible(true);
+            
+        });
+    }
+}
