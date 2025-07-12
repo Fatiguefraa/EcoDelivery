5d0af868812d26f2f1bab5100046c0bb15df2f6e
ywacht <escuall1720@gmail.com>
2025-07-11 19:27:26 -0700

    crecionde clase comprueba
    es un codigo para comprobar las contrasenas

---
+++ src/main/java/com/mycompany/prueba/comprobavarc.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author Ramon
+ */
+public class comprobavarc {
+    public class ComprobarVarC {
+
+    // Array con 5 contraseñas válidas
+    private String[] contrasenasValidas = {
+        "clave123",
+        "admin2025",
+        "segura456",
+        "usuario789",
+        "password1"
+    };
+
+    // Método para comprobar la contraseña ingresada en el textfield de Login
+   /* public boolean comprobarContrasena(Login login) {
+        String contrasenaIngresada = login.contrasena_input.getText();
+
+        for (String contrasena : contrasenasValidas) {
+            if (contrasena.equals(contrasenaIngresada)) {
+                return true; // La contraseña es válida
+            }
+        }
+
+        return false; // No se encontró coincidencia
+    }
+}
+
+}*/
